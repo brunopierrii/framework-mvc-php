075f7dcb6449f30a5442eb1abc4e475c76547efb
Bruno Pierri <pierri95@gmail.com>
2023-11-28 17:29:42 -0300

    created abstract class, layer where it starts and mananges routes

--- core/RouterCore.php
+++ core/RouterCore.php
@@ -2,10 +2,45 @@
 
 namespace Core;
 
-class RouterCore 
+abstract class RouterCore
 {
+    private $routes;
+
+    abstract protected function createRoutes();
+
     public function __construct()
     {
-        echo 'Router Core';
+        $this->createRoutes()
+            ->start();
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    protected function currentPath()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+    protected function start()
+    {
+        foreach ($this->getRoutes() as $key => $route) {
+            $variablePath = substr($this->currentPath(), strlen('/framework-mvc-php'));
+            if($variablePath == $route['path'])
+            {
+                $class = 'App\\Controller\\' . ucfirst($route['controller']);
+                $controller = new $class;
+                $action = $route['action'];
+                $controller->$action();
+            }
+        }
+
     }
 }
