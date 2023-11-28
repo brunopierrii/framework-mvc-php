4ded2b27ca4e5c8629dccd9d94602e960495525d
Bruno Pierri <pierri95@gmail.com>
2023-11-28 17:27:28 -0300

    created class for create routes

--- config/Router.php
+++ config/Router.php
@@ -1,3 +1,21 @@
 <?php
 
+namespace Config;
 
+use Core\RouterCore;
+
+class Router extends RouterCore
+{
+    protected function createRoutes()
+    {
+        $routes['index'] = [
+            'path' => '/',
+            'controller' => 'HomeController',
+            'action' => 'index'
+        ];
+
+        $this->setRoutes($routes);
+
+        return $this;
+    }
+}
