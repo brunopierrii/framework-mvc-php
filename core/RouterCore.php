86755a451de4ec87d16270044178a2b9d13cac05
Bruno Pierri <pierri95@gmail.com>
2023-11-29 00:32:56 -0300

    change in the way the routes is initialized and communicated with the controller

--- core/RouterCore.php
+++ core/RouterCore.php
@@ -4,43 +4,122 @@ namespace Core;
 
 abstract class RouterCore
 {
-    private $routes;
+    private $uri;
+    private $method;
+    private $routesArr;
 
     abstract protected function createRoutes();
 
     public function __construct()
     {
-        $this->createRoutes()
-            ->start();
+        $this->start()
+             ->createRoutes()
+             ->execute();
     }
 
     public function setRoutes(array $routes)
     {
-        $this->routes = $routes;
+        $this->routesArr = $routes;
     }
 
     public function getRoutes()
     {
-        return $this->routes;
+        return $this->routesArr;
+    }
+
+    protected function get(string $path, array $action = null)
+    {
+        $this->routesArr[] = [
+            'path' => $path,
+            'controller' => $action[0],
+            'action' => $action[1],
+            'method' => 'GET'
+        ];
+    }
+
+    protected function post(string $path, array $action)
+    {
+        $this->routesArr[] = [
+            'path' => $path,
+            'controller' => $action[0],
+            'action' => $action[1],
+            'method' => 'POST'
+        ];
+    }
+
+    protected function start()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+
+        $uri = $_SERVER['REQUEST_URI'];
+
+        if(strpos($uri, '?')) {
+            $uri = mb_substr($uri, 0, strpos($uri, '?'));
+        }
+
+        $uriExplode = explode('/', $uri);
+
+        $uri = self::uriFormat($uriExplode);
+
+        unset($uri[0]);
+
+        $this->uri = implode('/', self::uriFormat($uri));
+
+        return $this;
+    }
+
+    private function execute()
+    {
+        foreach ($this->getRoutes() as $route) {
+            if ($this->method !== $route['method']) {
+                
+                header('HTTP/1.1 405 Method Not Allowed');
+                header('Allow: ' . $route['method']);
+                echo 'Erro 405: Método HTTP incorreto. Use ' . $route['method'];
+
+                return;
+            }
+
+            $path = substr($route['path'], 1);
+
+            if(substr($path, -1) == '/') {
+                $path = substr($path, 0, -1);
+            }
+
+            if($path == $this->uri) {
+                $this->executeController($route['controller'], $route['action']);
+            } else {
+                echo 'route not found';
+            }
+
+        }
+    }
+
+    private function executeController(string $controller, string $action)
+    {
+        $class = 'App\\Controller\\' . ucfirst($controller);
+
+        if(!class_exists($class)) {
+            echo 'Class not found';
+            return;
+        }
+
+        if(!method_exists($class, $action)) {
+            echo 'Function not found';
+            return;
+        }
+        
+        $object = new $class;
+        $object->$action();
+    }
+
+    private static function uriFormat(array $arr)
+    {
+        return array_values(array_filter($arr));
     }
 
     protected function currentPath()
     {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    }
-
-    protected function start()
-    {
-        foreach ($this->getRoutes() as $key => $route) {
-            $variablePath = substr($this->currentPath(), strlen('/framework-mvc-php'));
-            if($variablePath == $route['path'])
-            {
-                $class = 'App\\Controller\\' . ucfirst($route['controller']);
-                $controller = new $class;
-                $action = $route['action'];
-                $controller->$action();
-            }
-        }
-
     }
 }
