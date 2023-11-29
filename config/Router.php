c709f957e18d988312d5bcbb0d986d873eb502f2
Bruno Pierri <pierri95@gmail.com>
2023-11-29 00:30:29 -0300

    change in the way routes are created

--- config/Router.php
+++ config/Router.php
@@ -8,14 +8,6 @@ class Router extends RouterCore
 {
     protected function createRoutes()
     {
-        $routes['index'] = [
-            'path' => '/',
-            'controller' => 'HomeController',
-            'action' => 'index'
-        ];
-
-        $this->setRoutes($routes);
-
-        return $this;
+        $this->get('/', ['HomeController', 'index']);
     }
 }
