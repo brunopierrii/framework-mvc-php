6f18d48229dec050b67f04ee782b34f7890e4953
Bruno Pierri <pierri95@gmail.com>
2023-11-29 00:34:03 -0300

    insert new constant config in the file config.php, and import in the index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
+require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/config/config.php';
+require_once dirname(__DIR__).'/resources/functions/dump.php';
 
 use Config\Router;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
-require_once dirname(__DIR__).'/resources/functions/dump.php';
-
-$router = new Router();
-
+(new Router());
 
 
