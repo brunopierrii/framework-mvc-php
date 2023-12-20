252a1e97c20e66ae676ef6403b0437c8af543258
Bruno Pierri <pierri95@gmail.com>
2023-12-19 21:10:44 -0300

    composer updates

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'app/framework-mvc',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '265b5310d7e44a937d146991bb69e3e81f4c057a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'app/framework-mvc' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '265b5310d7e44a937d146991bb69e3e81f4c057a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
